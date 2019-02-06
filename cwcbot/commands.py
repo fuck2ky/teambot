471281b2612de2f194e0a8c7b9b6a524779ced1c
g19m02a92D <mvettosi>
2019-02-06 22:20:32 +0000

    removed ping, added weekend shortcut

--- cwcbot/commands.py
+++ cwcbot/commands.py
@@ -7,19 +7,32 @@ import calendar
 client = Bot(command_prefix='!')
 
 
+# Startup
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+
+# Bot Commands
 @client.command(pass_context=True)
 async def schedule(context, *args):
     message = context.message
     await message.delete()
-    # await context.send('{} arguments: {}'.format(len(args), ', '.join(args)))
     if args:
-        for named_day in args:
+        for named_day in expand_arguments(args):
             await schedule_day(context, named_day)
     else:
         today = date.today().weekday()
         for day in range(today, 7):
             await schedule_day(context, calendar.day_name[day])
-    await context.send('@everyone please react according to your availability')
+
+
+# Functions
+def run_bot():
+    client.run('NTQyMTI2NTkyNjIxODA1NTc4.DzpfSg.0saF4oIL0KIuUdweQl-zXaC6cOc')
 
 
 async def schedule_day(context, day):
@@ -29,14 +42,13 @@ async def schedule_day(context, day):
         await message.add_reaction('\N{THUMBS DOWN SIGN}')
 
 
-# Start
-@client.event
-async def on_ready():
-    print('Logged in as')
-    print(client.user.name)
-    print(client.user.id)
-    print('------')
-
-
-def run_bot():
-    client.run('NTQyMTI2NTkyNjIxODA1NTc4.DzpfSg.0saF4oIL0KIuUdweQl-zXaC6cOc')
+def expand_arguments(args):
+    args_expanded = []
+    for arg in args:
+        if arg == 'weekend':
+            args_expanded.append('Friday')
+            args_expanded.append('Saturday')
+            args_expanded.append('Sunday')
+        else:
+            args_expanded.append(arg)
+    return args_expanded
