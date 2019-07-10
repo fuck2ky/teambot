0525a7b54b651bed9a898cbc0658283d50316de3
Matteo Vettosi <matteo.vettosi@gmail.com>
2019-07-10 20:05:27 +0100

    Completed schedule formatting

--- cwcbot/cogs/schedule.py
+++ cwcbot/cogs/schedule.py
@@ -17,19 +17,19 @@ class ScheduleCog(commands.Cog):
         else:
             await self.schedule_day(context, calendar.day_name[4], 12)
             for day in range(5, 7):
-                await self.schedule_day(context, calendar.day_name[day])
+                await self.schedule_day(context, calendar.day_name[day], 9)
 
     async def schedule_day(self, context, day, start=0):
         await context.send(f'`{day}`')
 
-        times = [t for t in range(start, 24, 3)]
+        times = [t for t in range(start, 21, 3)]
 
         for time in times:
             full_time = (
                 f"EST {t_add(time, 0)} - {t_add(time, 3)}"
-                f"\nUK  {t_add(time, 5)} - {t_add(time, 8)}"
-                f"\nEU  {t_add(time, 6)} - {t_add(time, 9)}"
-                f"\nJAP {t_add(time, 15)} - {t_add(time, 18)}"
+                f" | UK  {t_add(time, 5)} - {t_add(time, 8)}"
+                f" | EU  {t_add(time, 6)} - {t_add(time, 9)}"
+                f" | JAP {t_add(time, 15)} - {t_add(time, 18)}"
             )
             message = await context.send(full_time)
             await message.add_reaction('\N{THUMBS UP SIGN}')
