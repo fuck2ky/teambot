09a5955eb52d8a58ec62f1d788130378e23be658
Matteo Vettosi <matteo.vettosi@gmail.com>
2019-07-10 21:28:34 +0100

    Restructured for scheduled tasks addition

--- cwcbot/cogs/schedule.py
+++ cwcbot/cogs/schedule.py
@@ -7,44 +7,53 @@ class ScheduleCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(pass_context=True)
-    async def schedule(self, context, *args):
+    @commands.command(name='schedule', pass_context=True)
+    async def schedule_match(self, context, *args):
         message = context.message
         await message.delete()
         if args:
             for named_day in expand_arguments(args):
-                await self.schedule_day(context, named_day)
+                await schedule_day(context, named_day)
         else:
-            await self.schedule_day(context, calendar.day_name[4], 12)
-            for day in range(5, 7):
-                await self.schedule_day(context, calendar.day_name[day], 9)
+            await schedule_weekend(context)
 
-    async def schedule_day(self, context, day, start=0):
-        await context.send(f'`{day}`')
+# Periodic tasks
 
-        times = [t for t in range(start, 21, 3)]
 
-        for time in times:
-            full_time = (
-                f"EST {t_add(time, 0)} - {t_add(time, 3)}"
-                f" | UK  {t_add(time, 5)} - {t_add(time, 8)}"
-                f" | EU  {t_add(time, 6)} - {t_add(time, 9)}"
-                f" | JAP {t_add(time, 15)} - {t_add(time, 18)}"
-            )
-            message = await context.send(full_time)
-            await message.add_reaction('\N{THUMBS UP SIGN}')
-            await message.add_reaction('\N{THUMBS DOWN SIGN}')
+# Module level functions
+async def schedule_weekend(context):  # TODO switch from context to channel
+    await schedule_day(context, calendar.day_name[4], 12)
+    for day in range(5, 7):
+        await schedule_day(context, calendar.day_name[day], 9)
 
-    def expand_arguments(self, args):
-        args_expanded = []
-        for arg in args:
-            if arg == 'weekend':
-                args_expanded.append(calendar.day_name[5])
-                args_expanded.append(calendar.day_name[6])
-                args_expanded.append(calendar.day_name[7])
-            else:
-                args_expanded.append(arg)
-        return args_expanded
+
+async def schedule_day(context, day, start=0):
+    await context.send(f'`{day}`')
+
+    times = [t for t in range(start, 21, 3)]
+
+    for time in times:
+        full_time = (
+            f"EST {t_add(time, 0)} - {t_add(time, 3)}"
+            f" | UK  {t_add(time, 5)} - {t_add(time, 8)}"
+            f" | EU  {t_add(time, 6)} - {t_add(time, 9)}"
+            f" | JAP {t_add(time, 15)} - {t_add(time, 18)}"
+        )
+        message = await context.send(full_time)
+        await message.add_reaction('\N{THUMBS UP SIGN}')
+        await message.add_reaction('\N{THUMBS DOWN SIGN}')
+
+
+def expand_arguments(args):
+    args_expanded = []
+    for arg in args:
+        if arg == 'weekend':
+            args_expanded.append(calendar.day_name[5])
+            args_expanded.append(calendar.day_name[6])
+            args_expanded.append(calendar.day_name[7])
+        else:
+            args_expanded.append(arg)
+    return args_expanded
 
 
 def t_add(time, to_add):
