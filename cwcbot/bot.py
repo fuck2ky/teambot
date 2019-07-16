0afc21f29f4086e4644a8d2cdbd1de6429387da6
Matteo Vettosi <matteo.vettosi@gmail.com>
2019-07-16 01:22:05 +0100

    added practice event

--- cwcbot/bot.py
+++ cwcbot/bot.py
@@ -2,10 +2,17 @@
 
 from discord.ext.commands import Bot
 from datetime import date
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from cwcbot.events.base_event import Event
+from cwcbot.events import *
 import json
 import os
 import sys
 import traceback
+
+
+# Scheduler that will be used to manage events
+sched = AsyncIOScheduler()
 
 
 # Load configurations
@@ -16,20 +23,36 @@ with open(config_file_path) as config_file:
 
 
 # Instantiate bot
+print("\nLoading commands...")
 initial_extensions = ['cwcbot.cogs.schedule']
 client = Bot(command_prefix='!')
+n_cmd = 0
 for extension in initial_extensions:
     try:
         client.load_extension(extension)
+        n_cmd += 1
     except Exception as e:
         print(f'Failed to load extension {extension}.', file=sys.stderr)
         traceback.print_exc()
+print(f"{n_cmd} commands loaded")
+
+
+# Load all events
+print("\nLoading events...")
+n_ev = 0
+for ev in Event.__subclasses__():
+    event = ev()
+    sched.add_job(event.run, 'interval', (client, config,),
+                  seconds=event.interval_minutes)
+    n_ev += 1
+sched.start()
+print(f"{n_ev} events loaded")
 
 
 # Startup
 @client.event
 async def on_ready():
-    print('Logged in as')
+    print('\nLogged in as')
     print(client.user.name)
     print(client.user.id)
     print('------')
